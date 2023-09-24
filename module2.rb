29b335519fa3b8aee72659988bea7aa57963c976
imaisachiyo <imaisachiyo.1030@gmail.com>
2023-09-24 15:42:50 +0900

    モジュールについて学んだ

---
+++ module2.rb
@@ -0,0 +1,33 @@
+module WhippedCream #WhippedCreamモジュールを作成
+
+#インスタンスメソッドwhipped_creamを呼ぶとインスタンス変数＠nameの後ろにホイップクリームを足して返す
+def whipped_cream
+    @name += "ホイップクリーム"
+end
+
+end
+
+class Drink
+
+   include WhippedCream 
+   #DrinkクラスのオブジェクトはモジュールWhippedCreamのwhipped_creamを使えるようになる
+
+   def initialize(name)
+    @name = name
+   end
+   def name
+    @name
+   end
+
+end
+
+#引数にカフェモカが渡されて、whipped_creamメソッドにて＠nameに代入
+drink = Drink.new("カフェモカ") 
+drink.whipped_cream
+#whipped_creamが呼び出されて＠nameの末尾にホイップクリームが追加
+
+puts drink.name
+#@nameは上記の流れで＠nameの末尾にホイップクリームが追加が格納されているので
+#=> カフェモカホイップクリームが出力される
+
+
