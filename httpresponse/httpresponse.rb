068a61e226c0eb29d33cdf138b75aac934573a01
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-01-04 21:09:19 +0900

    仮

--- httpresponse/httpresponse.rb
+++ httpresponse/httpresponse.rb
@@ -1,13 +1,21 @@
-require 'net/http'
+class HttpResponce
 
-uri = "https://github.com/okaka-cccc"
-response = Net::HTTP.get_response(URI.parse(uri))
-puts response.code 
-puts "============"
-
-begin
-    puts response.value
-  rescue => e
-    puts e.class # => Net::HTTPServerException
-    puts e.message # => 404 "Not Found"
+  def name_not_exist_as_github_account
+    require 'net/http'
+    account_name = "okaka-cccc"
+    uri = "https://github.com/#{account_name}"
+    response = Net::HTTP.get_response(URI.parse(uri))
+    puts uri
+    puts response.code 
+    puts "========"
+    begin
+      puts response.value
+    rescue => e
+      puts e.class # => Net::HTTPServerException
+      puts e.message # => 404 "Not Found"
+    end
   end
+end 
+
+httpresponce = HttpResponce.new
+httpresponce.name_not_exist_as_github_account
